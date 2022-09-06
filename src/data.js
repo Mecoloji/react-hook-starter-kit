1824b61a9acfb3527bedf34810daf48c5d7c3bc8
-mM <mecoloji1@outlook.com>
2022-09-06 16:18:17 +0300

    useRef & useReducer updated

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const data = [
-    { id: 1, name: 'murat' },
-    { id: 2, name: 'namık' },
-    { id: 3, name: 'suzan' },
-    { id: 4, name: 'eda' },
-]
+  { id: 1, name: "murat" },
+  { id: 2, name: "namık" },
+  { id: 3, name: "suzan" },
+  { id: 4, name: "eda" },
+];
